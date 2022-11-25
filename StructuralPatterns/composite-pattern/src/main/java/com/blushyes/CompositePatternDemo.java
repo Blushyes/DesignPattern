a4325446b0da059ae88e61c5eefadbb93ba5ad0e
Blushyes <2671431017@qq.com>
2022-11-26 00:35:48 +0800

    更新了组合模式

---
+++ StructuralPatterns/composite-pattern/src/main/java/com/blushyes/CompositePatternDemo.java
@@ -0,0 +1,40 @@
+package com.blushyes;
+
+import com.blushyes.Employee;
+
+public class CompositePatternDemo {
+    public static void main(String[] args) {
+        //树形结构用组合模式
+        //这里先用菜鸟的代码
+        //后续再补上
+        Employee CEO = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //打印该组织的所有员工
+        System.out.println(CEO);
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
